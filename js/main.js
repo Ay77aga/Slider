6dd2ab42905dbc01b985797e6dffc44167bff600
Bassel <bassel444555@gmail.com>
2022-01-12 10:01:02 +0200

    update

--- js/main.js
+++ js/main.js
@@ -1,49 +1,51 @@
-let imgsSrc = [];
-let img = '../imgs/img';
-let imgs = 7;
-for (let i = 0; i <= imgs; i++) {
-  imgsSrc.push(img + i + '.jpg');
-}
-// console.log(imgsSrc.length);
-let next_btn = document.querySelector('.Next');
-let prev_btn = document.querySelector('.Prev');
-let Preview = document.querySelector('.container img');
-let count = 0;
-let slide = document.querySelector('.slider');
-let container = document.querySelector('.container');
-let counter = container.setAttribute('data-index', `${count +1}/${imgsSrc.length}`);
-
-
-
-prev_btn.style.display = 'none';
-
-Preview.src = imgsSrc[0];
-next_btn.addEventListener('click', () => {
-  count++;
-  if (count > 0) {
-    prev_btn.style.display = 'block';
+window.addEventListener('load', () => {
+  let imgsSrc = [];
+  let img = '../imgs/img';
+  let imgs = 7;
+  for (let i = 0; i <= imgs; i++) {
+    imgsSrc.push(img + i + '.jpg');
   }
-  if (count >= imgsSrc.length - 1) {
-    next_btn.style.display = 'none';
-  }
+  // console.log(imgsSrc.length);
+  let next_btn = document.querySelector('.Next');
+  let prev_btn = document.querySelector('.Prev');
+  let Preview = document.querySelector('.container img');
+  let count = 0;
+  let slide = document.querySelector('.slider');
+  let container = document.querySelector('.container');
+  let counter = container.setAttribute('data-index', `${count +1}/${imgsSrc.length}`);
 
-  // console.log(count);
-  counter = container.setAttribute('data-index', `${count +1}/${imgsSrc.length}`);
-  Preview.src = imgsSrc[count];
-  slide.style.background = `url(${imgsSrc[count]})`
-});
-prev_btn.addEventListener('click', () => {
-  count--;
-  if (count <= 0) {
-    prev_btn.style.display = 'none';
-  }
-  if (count <= imgsSrc.length - 1) {
-    next_btn.style.display = 'block';
-  }
 
-  counter = container.setAttribute('data-index', `${count +1}/${imgsSrc.length}`);
-  Preview.src = imgsSrc[count];
-  slide.style.background = `url(${imgsSrc[count]})`
-  console.log(count);
 
+  prev_btn.style.display = 'none';
+
+  Preview.src = imgsSrc[0];
+  next_btn.addEventListener('click', () => {
+    count++;
+    if (count > 0) {
+      prev_btn.style.display = 'block';
+    }
+    if (count >= imgsSrc.length - 1) {
+      next_btn.style.display = 'none';
+    }
+
+    // console.log(count);
+    counter = container.setAttribute('data-index', `${count +1}/${imgsSrc.length}`);
+    Preview.src = imgsSrc[count];
+    slide.style.background = `url(${imgsSrc[count]})`
+  });
+  prev_btn.addEventListener('click', () => {
+    count--;
+    if (count <= 0) {
+      prev_btn.style.display = 'none';
+    }
+    if (count <= imgsSrc.length - 1) {
+      next_btn.style.display = 'block';
+    }
+
+    counter = container.setAttribute('data-index', `${count +1}/${imgsSrc.length}`);
+    Preview.src = imgsSrc[count];
+    slide.style.background = `url(${imgsSrc[count]})`
+    console.log(count);
+
+  });
 });
